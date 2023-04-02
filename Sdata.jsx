b9d5babc76f66a97e159460ee98f761cd343bdb3
SimranAhuja00 <97106836+SimranAhuja00@users.noreply.github.com>
2023-04-02 16:44:46 +0530

    Add files via upload

---
+++ Sdata.jsx
@@ -0,0 +1,35 @@
+const Sdata = [
+
+    {
+        sname: "Black Mirror",
+        imgscr: "https://wallpapercave.com/w/wp4786354.jpg",
+        title: "Netflix Original Series",
+        links:  "https://www.netflix.com/in/title/70264888"
+    },
+    {
+        sname: "Stranger Things",
+        imgscr: "https://wallpapercave.com/w/wp11827124.jpg",
+        title: "Netflix Original Series",
+        links:  "https://www.netflix.com/in/title/80057281"
+    },
+    {
+        sname: "Atypical",
+        imgscr: "https://wallpapercave.com/w/wp3920138.jpg",
+        title: "Netflix Original Series",
+        links:  "https://www.netflix.com/in/title/80117540"
+    },
+    {
+        sname: "Friends",
+        imgscr: "https://wallpapercave.com/w/wp5149318.jpg",
+        title: "Netflix Original Series",
+        links:  "https://www.netflix.com/in/title/70153404"
+    },
+    {
+        sname: "The Office",
+        imgscr: "https://wallpapercave.com/w/wp5941640.jpg",
+        title: "Netflix Original Series",
+        links:  "https://www.netflix.com/in/title/70136120"
+    },
+];
+
+export default Sdata;
